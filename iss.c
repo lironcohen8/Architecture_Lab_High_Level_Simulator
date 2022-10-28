4fbbf4e2e95346a224e4d67270111b4ccc63d9df
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-10-28 23:36:17 +0300

    fixed compilation

--- iss.c
+++ iss.c
@@ -30,6 +30,16 @@
 #define MEM_SIZE	(1 << MEM_SIZE_BITS)
 #define MEM_MASK	(MEM_SIZE - 1)
 unsigned int mem[MEM_SIZE];
+
+typedef struct {
+    unsigned int opcode;
+    unsigned int dst;
+    unsigned int src0;
+    unsigned int src1;
+    signed int imm;
+    unsigned int raw_cmd;
+} command;
+
 unsigned int program_length = 0;
 unsigned int inst_cnt = 0;
 bool is_halt = false;
@@ -51,30 +61,21 @@ char *input_file_name;
 #define IMM_MASK 0x0000FFFF
 #define SIGN_EXT_MASK 0x00008000
 
-typedef struct {
-    int8_t opcode;
-    int8_t dst;
-    int8_t src0;
-    int8_t src1;
-    int16_t imm;
-    char* raw_cmd;
-} command;
-
 int pc = 0;
 
 /* opens input_and_output files */
 static void open_input_and_output_files() {
-	fopen(&input_file, input_file_name, "r");
+	input_file = fopen(input_file_name, "r");
     if (input_file == NULL) {
         printf("error opening file %s\n", input_file_name);
     }
 
-	fopen(&trace_file, "trace.txt", "w");
+	trace_file = fopen("trace.txt", "w");
 	if (trace_file == NULL) {
         printf("error opening trace file\n");
     }
 
-	fopen(&sram_out_file, "sram_out.txt", "w");
+	sram_out_file = fopen("sram_out.txt", "w");
 	if (sram_out_file == NULL) {
         printf("error opening sram out file\n");
     }
@@ -84,9 +85,9 @@ static void open_input_and_output_files() {
 static void load_memory_from_input_file() {
 	char line_buffer[8];
 
-	while (getline(&line_buffer, 8, input_file) != -1) {
-    	mem[program_length++] = line_buffer;
-	}
+	while (fgets(line_buffer, 8 + 2, input_file) != NULL) {
+        sscanf_s(line_buffer, "%X", &mem[program_length++]);
+    }
 }
 
 /* writes the first lineof the trace file */
@@ -94,39 +95,25 @@ static void trace_first_line() {
 	fprintf(trace_file, "program %s loaded, %d lines\n\n", input_file_name, program_length);
 }
 
-/* constructs the command from the input line */
-static void parse_command() {
-	int32_t row;
-	sprintf(row, "%d", mem[pc]);
-
-    cmd->opcode = row & 0xFFF;
-    cmd->dst = (row >> OPCODE_SHIFT) & OPCODE_MASK;
-    cmd->src0 = (row >> SRC0_SHIFT) & SRC0_MASK;
-    cmd->src1 = (row >> SRC1_SHIFT) & SRC1_MASK;
-    cmd->imm = (row) & IMM_MASK;
-    cmd->raw_cmd = row;
-
-	regs[1] = sign_ext_imm(cmd->imm);
+/* sign extention the imm */
+static int sign_ext_imm(int imm) {
+	// if we need to extend with 1s
+	if (SIGN_EXT_MASK & imm) {
+        return imm + 0xFFFF0000;
+    }
+	return imm;
 }
 
-/* executes the relevant command by opcode */
-static void exec_command() {
-	if (cmd->opcode == ADD) run_add_cmd();
-	else if (cmd->opcode == SUB) run_sub_cmd();
-	else if (cmd->opcode == LSF) run_lsf_cmd();
-	else if (cmd->opcode == RSF) run_rsf_cmd();
-	else if (cmd->opcode == AND) run_and_cmd();
-	else if (cmd->opcode == OR) run_or_cmd();
-	else if (cmd->opcode == XOR) run_xor_cmd();
-	else if (cmd->opcode == LHI) run_lhi_cmd();
-	else if (cmd->opcode == LD) run_ld_cmd();
-	else if (cmd->opcode == ST) run_st_cmd();
-	else if (cmd->opcode == JLT) run_jlt_cmd();
-	else if (cmd->opcode == JLE) run_jle_cmd();
-	else if (cmd->opcode == JEQ) run_jeq_cmd();
-	else if (cmd->opcode == JNE) run_jne_cmd();
-	else if (cmd->opcode == JIN) run_jin_cmd();
-	else is_halt = true; // halt
+/* constructs the command from the input line */
+static void parse_command() {
+    cmd->opcode = (mem[pc] >> OPCODE_SHIFT) & OPCODE_MASK;
+    cmd->dst = (mem[pc] >> DST_SHIFT) & DST_MASK;
+    cmd->src0 = (mem[pc] >> SRC0_SHIFT) & SRC0_MASK;
+    cmd->src1 = (mem[pc] >> SRC1_SHIFT) & SRC1_MASK;
+    cmd->imm = (mem[pc]) & IMM_MASK;
+    cmd->raw_cmd = mem[pc];
+
+	regs[1] = sign_ext_imm(cmd->imm);
 }
 
 static void run_add_cmd() {
@@ -199,46 +186,36 @@ static void run_jne_cmd() {
 
 static void run_jin_cmd() {
 	regs[7] = pc;
-	pc = cmd->regs[cmd->src0];
+	pc = regs[cmd->src0];
+}
+
+/* executes the relevant command by opcode */
+static void exec_command() {
+	if (cmd->opcode == ADD) run_add_cmd();
+	else if (cmd->opcode == SUB) run_sub_cmd();
+	else if (cmd->opcode == LSF) run_lsf_cmd();
+	else if (cmd->opcode == RSF) run_rsf_cmd();
+	else if (cmd->opcode == AND) run_and_cmd();
+	else if (cmd->opcode == OR) run_or_cmd();
+	else if (cmd->opcode == XOR) run_xor_cmd();
+	else if (cmd->opcode == LHI) run_lhi_cmd();
+	else if (cmd->opcode == LD) run_ld_cmd();
+	else if (cmd->opcode == ST) run_st_cmd();
+	else if (cmd->opcode == JLT) run_jlt_cmd();
+	else if (cmd->opcode == JLE) run_jle_cmd();
+	else if (cmd->opcode == JEQ) run_jeq_cmd();
+	else if (cmd->opcode == JNE) run_jne_cmd();
+	else if (cmd->opcode == JIN) run_jin_cmd();
+	else is_halt = true; // halt
 }
 
 /* returns true if a command is a jump command and false otherwise */
 static bool is_jump_cmd() {
-	return cmd->opcode == JLT |  
-		   cmd->opcode == JLE | 
-		   cmd->opcode == JEQ | 
-		   cmd->opcode == JNE | 
-		   cmd->opcode == JIN;
-}
-
-/* sign extention the imm */
-static int sign_ext_imm(int imm) {
-	// if we need to extend with 1s
-	if (SIGN_EXT_MASK & imm) {
-        return imm + 0xFFFF0000;
-    }
-	return imm;
-}
-
-/* traces command to trace file */
-static void trace_command() {
-	fprintf(trace_file, "--- instruction %i (%04x) @ PC %d (%04x) -----------------------------------------------------------\n", inst_cnt, inst_cnt, pc, pc);
-    fprintf(trace_file, "pc = %04d, inst = %08x, opcode = %d (%s), ", pc, cmd->raw_cmd, cmd->opcode, get_opcode_str());
-    fprintf(trace_file, "dst = %d, src0 = %d, src1 = %d, immediate = %08x\n", cmd->dst, cmd->src0, cmd->src1, cmd->imm);
-    fprintf(trace_file, "r[0] = %08x r[1] = %08x r[2] = %08x r[3] = %08x \nr[4] = %08x r[5] = %08x r[6] = %08x r[7] = %08x \n\n", regs[0], regs[1], regs[2], regs[3], regs[4], regs[5], regs[6], regs[7]);
-
-    if ((cmd->opcode == ADD) || (cmd->opcode == SUB) || (cmd->opcode == LSF) || (cmd->opcode == RSF) || (cmd->opcode == AND) || (cmd->opcode == OR) || (cmd->opcode == XOR) || (cmd->opcode == LHI)) {
-        fprintf(trace_file, ">>>> EXEC: R[%d] = %d %s %d <<<<\n\n", cmd->dst, regs[cmd->src0], get_curr_opcode_str(), regs[cmd->src1]);
-    }
-    else if (cmd->opcode == LD) {
-        fprintf(trace_file, ">>>> EXEC: R[%d] = MEM[%d] = %08x <<<<\n\n", cmd->dst, regs[cmd->src1], regs[cmd->src1]);
-    }
-    else if (cmd->opcode == ST) {
-        fprintf(trace_file, ">>>> EXEC: MEM[%d] = R[%d] = %08x <<<<\n\n", regs[cmd->src1], cmd->drc0, regs[cmd->src0]);
-    }
-    else if (cmd->opcode == HLT) {
-        fprintf(trace_file, ">>>> EXEC: HALT at PC %04x<<<<\n", pc);
-    }
+	return (cmd->opcode == JLT) |  
+		   (cmd->opcode == JLE) | 
+		   (cmd->opcode == JEQ) | 
+		   (cmd->opcode == JNE) | 
+		   (cmd->opcode == JIN);
 }
 
 /* returns string representation of current command's opcode */
@@ -261,6 +238,27 @@ static char* get_curr_opcode_str() {
     else if (opcode == JIN) return "JIN";
     else if (opcode == HLT) return "HLT";
     else                    return "";
+}
+
+/* traces command to trace file */
+static void trace_command() {
+	fprintf(trace_file, "--- instruction %i (%04x) @ PC %d (%04x) -----------------------------------------------------------\n", inst_cnt, inst_cnt, pc, pc);
+    fprintf(trace_file, "pc = %04d, inst = %08x, opcode = %d (%s), ", pc, cmd->raw_cmd, cmd->opcode, get_curr_opcode_str());
+    fprintf(trace_file, "dst = %d, src0 = %d, src1 = %d, immediate = %08x\n", cmd->dst, cmd->src0, cmd->src1, cmd->imm);
+    fprintf(trace_file, "r[0] = %08x r[1] = %08x r[2] = %08x r[3] = %08x \nr[4] = %08x r[5] = %08x r[6] = %08x r[7] = %08x \n\n", regs[0], regs[1], regs[2], regs[3], regs[4], regs[5], regs[6], regs[7]);
+
+    if ((cmd->opcode == ADD) || (cmd->opcode == SUB) || (cmd->opcode == LSF) || (cmd->opcode == RSF) || (cmd->opcode == AND) || (cmd->opcode == OR) || (cmd->opcode == XOR) || (cmd->opcode == LHI)) {
+        fprintf(trace_file, ">>>> EXEC: R[%d] = %d %s %d <<<<\n\n", cmd->dst, regs[cmd->src0], get_curr_opcode_str(), regs[cmd->src1]);
+    }
+    else if (cmd->opcode == LD) {
+        fprintf(trace_file, ">>>> EXEC: R[%d] = MEM[%d] = %08x <<<<\n\n", cmd->dst, regs[cmd->src1], regs[cmd->src1]);
+    }
+    else if (cmd->opcode == ST) {
+        fprintf(trace_file, ">>>> EXEC: MEM[%d] = R[%d] = %08x <<<<\n\n", regs[cmd->src1], cmd->src0, regs[cmd->src0]);
+    }
+    else if (cmd->opcode == HLT) {
+        fprintf(trace_file, ">>>> EXEC: HALT at PC %04x<<<<\n", pc);
+    }
 }
 
 /* updates PC for non-jump commands */
