70f12a21865e4bbe19a8128e2bc0b84d3e433626
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-10-30 13:06:24 +0200

    added h file

--- iss.c
+++ iss.c
@@ -4,42 +4,8 @@
  * Yuval Mor 209011543
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stdint.h>
-
-#define ADD 0
-#define SUB 1
-#define LSF 2
-#define RSF 3
-#define AND 4
-#define OR  5
-#define XOR 6
-#define LHI 7
-#define LD 8
-#define ST 9
-#define JLT 16
-#define JLE 17
-#define JEQ 18
-#define JNE 19
-#define JIN 20
-#define HLT 24
-
-#define MEM_SIZE_BITS	(16)
-#define MEM_SIZE	(1 << MEM_SIZE_BITS)
-#define MEM_MASK	(MEM_SIZE - 1)
-unsigned int mem[MEM_SIZE];
-
-typedef struct {
-    unsigned int opcode;
-    unsigned int dst;
-    unsigned int src0;
-    unsigned int src1;
-    signed int imm;
-    unsigned int raw_cmd;
-} command;
-
+#include "iss.h"
+static unsigned int mem[MEM_SIZE];
 static unsigned int program_length = 0;
 static unsigned int inst_cnt = 0;
 static bool is_halt = false;
@@ -50,20 +16,8 @@ static FILE *trace_file;
 static FILE *sram_out_file;
 static FILE *input_file;
 static char *input_file_name;
-
-#define OPCODE_MASK 0x3E000000
-#define OPCODE_SHIFT 0x19
-#define DST_MASK 0x01C00000
-#define DST_SHIFT 0x16
-#define SRC0_MASK 0x00380000
-#define SRC0_SHIFT 0x13
-#define SRC1_MASK 0x00070000
-#define SRC1_SHIFT 0x10
-#define IMM_MASK 0x0000FFFF
-#define SIGN_EXT_MASK 0x00008000
-
-int pc = 0;
-int next_pc = 0;
+static int pc = 0;
+static int next_pc = 0;
 
 /* opens input_and_output files */
 static void open_input_and_output_files() {
---
+++ iss.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define ADD 0
+#define SUB 1
+#define LSF 2
+#define RSF 3
+#define AND 4
+#define OR  5
+#define XOR 6
+#define LHI 7
+#define LD 8
+#define ST 9
+#define JLT 16
+#define JLE 17
+#define JEQ 18
+#define JNE 19
+#define JIN 20
+#define HLT 24
+
+#define MEM_SIZE_BITS	(16)
+#define MEM_SIZE	(1 << MEM_SIZE_BITS)
+#define MEM_MASK	(MEM_SIZE - 1)
+
+#define OPCODE_MASK 0x3E000000
+#define OPCODE_SHIFT 0x19
+#define DST_MASK 0x01C00000
+#define DST_SHIFT 0x16
+#define SRC0_MASK 0x00380000
+#define SRC0_SHIFT 0x13
+#define SRC1_MASK 0x00070000
+#define SRC1_SHIFT 0x10
+#define IMM_MASK 0x0000FFFF
+#define SIGN_EXT_MASK 0x00008000
+
+typedef struct {
+    unsigned int opcode;
+    unsigned int dst;
+    unsigned int src0;
+    unsigned int src1;
+    signed int imm;
+    unsigned int raw_cmd;
+} command;
